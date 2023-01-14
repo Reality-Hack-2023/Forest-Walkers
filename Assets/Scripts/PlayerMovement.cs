93d207b590c69ac5f54677a7c137d07e52a88fb7
cingaldson <71413004+cingaldson@users.noreply.github.com>
2023-01-14 13:18:28 -0500

    Sound and Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f; // adjust the speed of the movement
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
+    }
+}
