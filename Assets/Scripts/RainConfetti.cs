6ed7f964a4d8df5c1f30622927a637bfa7a84b4a
Yashwanth <robinstrike8@gmail.com>
2023-01-15 12:39:33 -0500

    update

--- Assets/Scripts/RainConfetti.cs
+++ Assets/Scripts/RainConfetti.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class RainConfetti : MonoBehaviour
 {
     public static int _blobVisited;
+    public GameObject _confetti;
 
+    private void Awake()
+    {
+        _confetti.SetActive(false);
+    }
     private void Update()
     {
         if(_blobVisited>=4)
         {
             //RainConfetti
+            _confetti.SetActive(true);
         }
     }
 
